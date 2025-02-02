dfea8e029f9d82762f1a452cd25d7a73138f7e0e
ZincCat <zincchloride@outlook.com>
2025-02-02 01:11:08 -0500

    make small model loading work

--- gguf_parse.py
+++ gguf_parse.py
@@ -9,19 +9,20 @@ import torch
 import numpy as np
 from tqdm import tqdm
 import inspect
+from transformers.utils import ContextManagers
 from transformers.utils.logging import get_logger
 from transformers.integrations import (
     GGUF_CONFIG_MAPPING,
     GGUF_TOKENIZER_MAPPING,
     _gguf_parse_value,
 )
-from transformers import PreTrainedModel, PretrainedConfig
+from transformers import PretrainedConfig, Qwen2Model
+from transformers.modeling_utils import no_init_weights, init_empty_weights
 from gguf import MODEL_ARCH_NAMES, get_tensor_name_map, GGUFReader, dequantize
-from accelerate.utils import (
-        offload_weight,
-        set_module_tensor_to_device
-    )
+from accelerate.utils import offload_weight, set_module_tensor_to_device
 
+# set random seed for reproducibility
+torch.manual_seed(0)
 
 logger = get_logger(__name__)
 
@@ -32,7 +33,10 @@ GGUF_TO_TRANSFORMERS_MAPPING = {
             "tensor_count": "tensor_count",
             "kv_count": "kv_count",
         },
-        "general": {"file_type": "file_type", "quantization_version": "quantization_version"},
+        "general": {
+            "file_type": "file_type",
+            "quantization_version": "quantization_version",
+        },
     },
     "config": GGUF_CONFIG_MAPPING,
     "tokenizer": {"tokenizer": GGUF_TOKENIZER_MAPPING["tokenizer"]},
@@ -41,10 +45,12 @@ GGUF_TO_TRANSFORMERS_MAPPING = {
 
 GGUF_SUPPORTED_ARCHITECTURES = list(GGUF_TO_TRANSFORMERS_MAPPING["config"].keys())
 
+
 class GGUFTensor(NamedTuple):
     weights: np.ndarray
     name: str
     metadata: dict
+
 
 class TensorProcessor:
     def __init__(self, config=None):
@@ -52,6 +58,7 @@ class TensorProcessor:
 
     def process(self, weights, name, **kwargs):
         return GGUFTensor(weights, name, {})
+
 
 class Qwen2MoeTensorProcessor(TensorProcessor):
     def __init__(self, config=None):
@@ -62,7 +69,9 @@ class Qwen2MoeTensorProcessor(TensorProcessor):
             tensor_key_mapping = kwargs.get("tensor_key_mapping")
             parsed_parameters = kwargs.get("parsed_parameters")
             if tensor_key_mapping:
-                self._split_moe_expert_tensor(weights, parsed_parameters, name, tensor_key_mapping)
+                self._split_moe_expert_tensor(
+                    weights, parsed_parameters, name, tensor_key_mapping
+                )
                 return GGUFTensor(weights, None, {})
         if "ffn_gate_inp_shexp" in name:
             # for compatibility tensor shared_expert_gate must be (1, 2048) dim,
@@ -71,7 +80,11 @@ class Qwen2MoeTensorProcessor(TensorProcessor):
         return GGUFTensor(weights, name, {})
 
     def _split_moe_expert_tensor(
-        self, weights: np.ndarray, parsed_parameters: Dict[str, Dict], name: str, tensor_key_mapping: dict
+        self,
+        weights: np.ndarray,
+        parsed_parameters: Dict[str, Dict],
+        name: str,
+        tensor_key_mapping: dict,
     ):
         # Original merge implementation
         # https://github.com/ggerganov/llama.cpp/blob/master/convert_hf_to_gguf.py#L1994-L2022
@@ -80,7 +93,9 @@ class Qwen2MoeTensorProcessor(TensorProcessor):
         for i in range(0, w_counter):
             temp_name = name.replace("mlp.experts.", f"mlp.experts.{i}.")
             exp_weight = weights[i]
-            parsed_parameters["tensors"][temp_name] = torch.from_numpy(np.copy(exp_weight))
+            parsed_parameters["tensors"][temp_name] = torch.from_numpy(
+                np.copy(exp_weight)
+            )
 
 
 TENSOR_PROCESSORS = {
@@ -94,9 +109,14 @@ TENSOR_PROCESSORS = {
     # "gemma2": Gemma2TensorProcessor,
 }
 
+
 def read_field(reader, field):
     value = reader.fields[field]
-    return [_gguf_parse_value(value.parts[_data_index], value.types) for _data_index in value.data]
+    return [
+        _gguf_parse_value(value.parts[_data_index], value.types)
+        for _data_index in value.data
+    ]
+
 
 def get_gguf_hf_weights_map(
     hf_model,
@@ -112,7 +132,6 @@ def get_gguf_hf_weights_map(
     See "Standardized tensor names" in
     https://github.com/ggerganov/ggml/blob/master/docs/gguf.md for details.
     """
-    print(hf_model.config)
     model_type = hf_model.config.model_type if model_type is None else model_type
     num_layers = hf_model.config.num_hidden_layers if num_layers is None else num_layers
     # hack: ggufs have a different name for cohere
@@ -147,7 +166,7 @@ def get_gguf_hf_weights_map(
         if hf_name.endswith(".weight") or hf_name.endswith(".bias"):
             name, suffix = hf_name.rsplit(".", 1)
             suffix = "." + suffix
-
+        name = "model." + name  # essential for the name_map.get_name to work
         gguf_name = name_map.get_name(name)
         if gguf_name is None:
             continue
@@ -156,16 +175,19 @@ def get_gguf_hf_weights_map(
 
     # Some model like Bloom converted from BloomModel instead of BloomForCausalLM
     # Therefore, we need to check submodule as well to get a correct mapping
-    if named_children := hf_model.named_children():
-        for name, child in named_children:
-            sub_map = get_gguf_hf_weights_map(child, model_type, num_layers, qual_name=f"{qual_name}{name}.")
-            # Ignore the keys that are already in the main map to avoid overwriting
-            sub_map = {k: v for k, v in sub_map.items() if k not in gguf_to_hf_name_map}
-            gguf_to_hf_name_map.update(sub_map)
+    # if named_children := hf_model.named_children():
+    #     for name, child in named_children:
+    #         sub_map = get_gguf_hf_weights_map(child, model_type, num_layers, qual_name=f"{qual_name}{name}.")
+    #         # Ignore the keys that are already in the main map to avoid overwriting
+    #         sub_map = {k: v for k, v in sub_map.items() if k not in gguf_to_hf_name_map}
+    #         gguf_to_hf_name_map.update(sub_map)
 
     return gguf_to_hf_name_map
 
-def load_gguf_checkpoint(gguf_checkpoint_path, return_tensors=False, model_to_load=None):
+
+def load_gguf_checkpoint(
+    gguf_checkpoint_path, return_tensors=False, model_to_load=None
+):
     """
     Load a GGUF file and return a dictionary of parsed parameters containing tensors, the parsed
     tokenizer and config attributes.
@@ -207,13 +229,21 @@ def load_gguf_checkpoint(gguf_checkpoint_path, return_tensors=False, model_to_lo
     if "stablelm" in architecture:
         attn_bias_name = {"attn_q.bias", "attn_k.bias", "attn_v.bias"}
         ffn_norm_name = "ffn_norm"
-        qkv_bias = any(bias_name in tensor.name for tensor in reader.tensors for bias_name in attn_bias_name)
-        use_parallel_residual = any(ffn_norm_name in tensor.name for tensor in reader.tensors)
+        qkv_bias = any(
+            bias_name in tensor.name
+            for tensor in reader.tensors
+            for bias_name in attn_bias_name
+        )
+        use_parallel_residual = any(
+            ffn_norm_name in tensor.name for tensor in reader.tensors
+        )
         parsed_parameters["config"]["use_qkv_bias"] = qkv_bias
         parsed_parameters["config"]["use_parallel_residual"] = not use_parallel_residual
 
     if architecture not in GGUF_SUPPORTED_ARCHITECTURES:
-        raise ValueError(f"GGUF model with architecture {architecture} is not supported yet.")
+        raise ValueError(
+            f"GGUF model with architecture {architecture} is not supported yet."
+        )
 
     # Handle tie_word_embeddings, if lm_head.weight is not present in tensors,
     # tie_word_embeddings is true otherwise false
@@ -228,7 +258,10 @@ def load_gguf_checkpoint(gguf_checkpoint_path, return_tensors=False, model_to_lo
         prefix = split[0]
         config_key = ".".join(split[1:])
 
-        value = [_gguf_parse_value(field.parts[_data_index], field.types) for _data_index in field.data]
+        value = [
+            _gguf_parse_value(field.parts[_data_index], field.types)
+            for _data_index in field.data
+        ]
 
         if len(value) == 1:
             value = value[0]
@@ -250,14 +283,18 @@ def load_gguf_checkpoint(gguf_checkpoint_path, return_tensors=False, model_to_lo
                     reader_keys.remove(gguf_key)
 
         if gguf_key in reader_keys:
-            logger.info(f"Some keys were not parsed and added into account {gguf_key} | {value}")
+            logger.info(
+                f"Some keys were not parsed and added into account {gguf_key} | {value}"
+            )
 
     # retrieve config vocab_size from tokenizer
     # Pleas refer to https://github.com/huggingface/transformers/issues/32526 for more details
     if "vocab_size" not in parsed_parameters["config"]:
         tokenizer_parameters = parsed_parameters["tokenizer"]
         if "tokens" in tokenizer_parameters:
-            parsed_parameters["config"]["vocab_size"] = len(tokenizer_parameters["tokens"])
+            parsed_parameters["config"]["vocab_size"] = len(
+                tokenizer_parameters["tokens"]
+            )
         else:
             logger.warning(
                 "Can't find a way to retrieve missing config vocab_size from tokenizer parameters. "
@@ -272,7 +309,9 @@ def load_gguf_checkpoint(gguf_checkpoint_path, return_tensors=False, model_to_lo
 
         ProcessorClass = TENSOR_PROCESSORS.get(architecture, TensorProcessor)
         processor = ProcessorClass(config=config)
-        for tensor in tqdm(reader.tensors, desc="Converting and de-quantizing GGUF tensors..."):
+        for tensor in tqdm(
+            reader.tensors, desc="Converting and de-quantizing GGUF tensors..."
+        ):
             name = tensor.name
             weights = dequantize(tensor.data, tensor.tensor_type)
 
@@ -297,6 +336,7 @@ def load_gguf_checkpoint(gguf_checkpoint_path, return_tensors=False, model_to_lo
         logger.info(f"Some keys of the GGUF file were not considered: {reader_keys}")
 
     return parsed_parameters
+
 
 def _load_state_dict_into_meta_model(
     model,
@@ -348,12 +388,19 @@ def _load_state_dict_into_meta_model(
 
         # We convert floating dtypes to the `dtype` passed except for float8_e4m3fn type. We also want to keep the buffers/params
         # in int/uint/bool and not cast them.
-        is_param_float8_e4m3fn = is_torch_e4m3fn_available and param.dtype == torch.float8_e4m3fn
-        if dtype is not None and torch.is_floating_point(param) and not is_param_float8_e4m3fn:
+        is_param_float8_e4m3fn = (
+            is_torch_e4m3fn_available and param.dtype == torch.float8_e4m3fn
+        )
+        if (
+            dtype is not None
+            and torch.is_floating_point(param)
+            and not is_param_float8_e4m3fn
+        ):
             if (
                 keep_in_fp32_modules is not None
                 and any(
-                    module_to_keep_in_fp32 in param_name.split(".") for module_to_keep_in_fp32 in keep_in_fp32_modules
+                    module_to_keep_in_fp32 in param_name.split(".")
+                    for module_to_keep_in_fp32 in keep_in_fp32_modules
                 )
                 and dtype == torch.float16
             ):
@@ -361,7 +408,9 @@ def _load_state_dict_into_meta_model(
 
                 # For backward compatibility with older versions of `accelerate`
                 # TODO: @sgugger replace this check with version check at the next `accelerate` release
-                if "dtype" in list(inspect.signature(set_module_tensor_to_device).parameters):
+                if "dtype" in list(
+                    inspect.signature(set_module_tensor_to_device).parameters
+                ):
                     set_module_kwargs["dtype"] = torch.float32
             else:
                 param = param.to(dtype)
@@ -404,43 +453,67 @@ def _load_state_dict_into_meta_model(
 
         if param_device == "disk":
             if not is_safetensors:
-                offload_index = offload_weight(param, param_name, offload_folder, offload_index)
+                offload_index = offload_weight(
+                    param, param_name, offload_folder, offload_index
+                )
         elif param_device == "cpu" and state_dict_index is not None:
-            state_dict_index = offload_weight(param, param_name, state_dict_folder, state_dict_index)
+            state_dict_index = offload_weight(
+                param, param_name, state_dict_folder, state_dict_index
+            )
         elif (
             not is_quantized
             or (not hf_quantizer.requires_parameters_quantization)
             or (
                 not hf_quantizer.check_quantized_param(
-                    model, param, param_name, state_dict, param_device=param_device, device_map=device_map
+                    model,
+                    param,
+                    param_name,
+                    state_dict,
+                    param_device=param_device,
+                    device_map=device_map,
                 )
             )
         ):
             # For backward compatibility with older versions of `accelerate` and for non-quantized params
-            set_module_tensor_to_device(model, param_name, param_device, **set_module_kwargs)
+            set_module_tensor_to_device(
+                model, param_name, param_device, **set_module_kwargs
+            )
         else:
-            hf_quantizer.create_quantized_param(model, param, param_name, param_device, state_dict, unexpected_keys)
+            hf_quantizer.create_quantized_param(
+                model, param, param_name, param_device, state_dict, unexpected_keys
+            )
 
     return error_msgs, offload_index, state_dict_index
 
 
 if __name__ == "__main__":
     # model = load_gguf_checkpoint("/home/zinccat/codes/OffloadTest/gg/qwen2.5-coder-0.5b-instruct-q4_0.gguf")
-    config = PretrainedConfig.from_pretrained("Qwen/Qwen2.5-Coder-0.5B")
-    gguf_path = "/home/zinccat/codes/OffloadTest/gg/qwen2.5-coder-0.5b-instruct-q4_0.gguf"
-    with torch.device("meta"):
-        dummy_model = PreTrainedModel(config)
-    # benchmark dequantization
-    from timeit import default_timer as timer
-    start = timer()
-    for _ in range(1):
-        state_dict = load_gguf_checkpoint(gguf_path, return_tensors=True, model_to_load=dummy_model)["tensors"]
-    # state_dict = load_gguf_checkpoint(gguf_path, return_tensors=True, model_to_load=dummy_model)["tensors"]
-    
-    print("Time elapsed:", timer() - start)
+    pretrained_model_name_or_path = "Qwen/Qwen2.5-Coder-0.5B"
+    config = PretrainedConfig.from_pretrained(pretrained_model_name_or_path)
 
+    # Path to the GGUF checkpoint
+    gguf_path = (
+        "models/qwen2.5-coder-0.5b-instruct-q4_0.gguf"
+    )
+
+    # Dummy model for loading state_dict
+    with torch.device("meta"):
+        dummy_model = Qwen2Model(config)
+    state_dict = load_gguf_checkpoint(
+        gguf_path, return_tensors=True, model_to_load=dummy_model
+    )["tensors"]
+
+    _fast_init = True
+
+    init_contexts = [no_init_weights(_enable=_fast_init), init_empty_weights()]
+
+    with ContextManagers(init_contexts):
+        # Let's make sure we don't run the init function of buffer modules
+        model = Qwen2Model(config)
+
+    # Error handling and loading state dict into model
     error_msgs, offload_index, state_dict_index = _load_state_dict_into_meta_model(
-        dummy_model,
+        model,
         state_dict,
         "",
         state_dict.keys(),
@@ -454,7 +527,15 @@ if __name__ == "__main__":
         is_safetensors=False,
         keep_in_fp32_modules=None,
         unexpected_keys=None,
-        pretrained_model_name_or_path=None,
+        pretrained_model_name_or_path=pretrained_model_name_or_path,
     )
-    print(error_msgs)
-    print(dummy_model)
+
+    print("Errors:", error_msgs)
+    print("Model:", model)
+
+    model.eval()
+
+    # Run a forward pass
+    with torch.no_grad():
+        # Adjust input dimensions according to your model's requirements (512 tokens in this case)
+        print(model(torch.randint(0, 151936, (1, 512))))
